ae7c080debbc75a55d8a60070ea4701e278c88e0
Gaga adi lesmana putra <angga121194@gmail.com>
2024-04-17 22:12:58 +0700

    update readme.md for detail information

--- README.md
+++ README.md
@@ -8,4 +8,44 @@ In the project directory, you can run:
 ### `npm install`
 ### `npm run dev`
 
+## Tech Stack & Others
+```bash
+- Framework Javascript : Nextjs & ReactJs Hooks
+- State Management : Redux
+- CSS pre processor : SASS/SCSS
+- Rest API : Axios
+- Git Repository :  Github
+- Responsive Layout : Dekstop, Mobile & Tablet
+- Live web can be accessed via internet
+https://evermos-project.vercel.app/
+- Include SEO for SSR (Server Side Rendering)
+```
+
+```bash
+# Pages in Evermos Project
+- Navigation (Navbar & Navbar Menu) 
+- Slider in Header Section
+- Show Product List
+- Show Product Detail
+- SEO Friendly
+- Rest API using Redux
+- Gallery Products
+- Animation in Product Detail when loading or refresh
+```
+
+## Base URL
+```bash
+There's two products to Rest API with Endpoint
+1. New Products:
+https://my-json-server.typicode.com/anggalof/evermos-project/ournewproducts
+2. Products
+https://my-json-server.typicode.com/anggalof/evermos-project/ourproducts
+
+GET Detail by ID
+Example:
+https://my-json-server.typicode.com/anggalof/evermos-project/ournewproducts/:id
+or
+https://my-json-server.typicode.com/anggalof/evermos-project/ourproducts/:id
+```
+
 
--- components/common/Navigation/index.js
+++ components/common/Navigation/index.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import Link from "next/link";
 import NavbarMenu from "../NavbarMenu";
 
 export const Navigation = () => {
@@ -48,42 +47,6 @@ export const Navigation = () => {
               </div>
             </div>
           </div>
-          
-          {/* <div className="nav-header">
-            <a href="/">
-              <img src="/images/evermos-logo.png" alt="logo" />
-            </a>
-          </div>
-          <div className="nav-btn">
-            <label htmlFor="nav-check">
-              <span></span>
-              <span></span>
-              <span></span>
-            </label>
-          </div>
-
-          <div className="nav-search-icon">
-            <i className="fa fa-search"></i>
-          </div>
-
-          <div className="nav-links">
-            <a href="/" target="_blank">Home</a>
-            <a href="/" target="_blank">Category</a>
-            <a href="/" target="_blank">About</a>
-            <a href="/" target="_blank">Contact</a>
-
-            <div className="nav-search">
-              <div className="form-search">
-                <i className="fa fa-search"></i>
-                <input
-                  type="text"
-                  name="search"
-                  className="form-control form-input"
-                  placeholder="Search something here!"
-                />
-              </div>
-            </div>
-          </div> */}
         </div>
       </div>
       <div className="rectangle-navbar"></div>
